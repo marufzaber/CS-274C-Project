46f2679833e95bd6021977b2a73391cfeeb3eef3
marufzaber <mzaber@uci.edu>
2020-05-22 11:58:53 -0700

    model_to_use now equals to whatever was passed on by argument

--- project/train/train.py
+++ project/train/train.py
@@ -3,9 +3,6 @@ import os
 
 from . import feedforward
 
-
-
-
 def get_args():
     """Argument parser.
 
@@ -13,27 +10,32 @@ def get_args():
       Dictionary of arguments.
     """
     parser = argparse.ArgumentParser()
+    
     parser.add_argument(
         '--job-dir',
         type=str,
         required=True,
         help='local or GCS location for writing checkpoints and exporting '
              'models')
+    
     parser.add_argument(
         '--num-epochs',
         type=int,
         default=16,
         help='number of times to go through the data, default=20')
+    
     parser.add_argument(
         '--batch-size',
         default=100,
         type=int,
         help='number of records to read during each training step, default=128')
+    
     parser.add_argument(
         '--learning-rate',
         default=.01,
         type=float,
         help='learning rate for gradient descent, default=.01')
+    
     parser.add_argument(
         '--nw-type',
         default='feedforward',
@@ -46,6 +48,6 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    # TODO: Switch based on --nw-type arg
-    model_to_use = feedforward
+
+    model_to_use = args.nw_type
     model_to_use.train(args.num_epochs, args.batch_size, args.learning_rate, args.job_dir)
