fab346836ae6cef78b2ab01c84199d25831152b7
marufzaber <mzaber@uci.edu>
2020-05-28 18:55:44 -0700

    plot generator added

--- project/tools/graph_generator.py
+++ project/tools/graph_generator.py
@@ -10,15 +10,15 @@ import matplotlib.pyplot as plt
 def generate(history_dict, file):
 	
 	loss_values = history_dict['loss']
-	val_loss_values = history_dict['accuracy']
-	epochs = range(1, len(acc) + 1)
-	plt.plot(epochs, loss_values, 'bo', label='Training loss')
-	plt.plot(epochs, val_loss_values, 'b', label='Training accuracy')
-	plt.title('Training and validation loss')
+	accuracy_values = history_dict['accuracy']
+	epochs = range(1, len(accuracy_values) + 1)
+	plt.plot(epochs, loss_values, 'bo', label='loss')
+	plt.plot(epochs, accuracy_values, 'b', label='accuracy')
+	plt.title('Training loss and accuracy')
 	plt.xlabel('Epochs')
 	plt.ylabel('Loss')
 	plt.legend()
 	plt.show()
 	plt.savefig(file)
 
-
+	
