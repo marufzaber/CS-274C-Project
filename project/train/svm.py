68dd7b6eeb0f216ca68671eb8f563a16e9c2887f
marufzaber <mzaber@uci.edu>
2020-05-31 15:58:39 -0700

    svm now acts fine

--- project/train/svm.py
+++ project/train/svm.py
@@ -7,17 +7,12 @@ from project.tools import graph_generator
 import multiprocessing.sharedctypes as sharedctypes
 import ctypes
 import tensorflow.keras as keras
-<<<<<<< HEAD
 from tensorflow.keras.layers import Dropout, Activation, Dense, Reshape, Flatten
 from keras.regularizers import l2
 import psutil
 from tensorflow.keras.layers.experimental import RandomFourierFeatures
 
 from keras import regularizers
-=======
-from tensorflow.keras.layers import Dropout, Activation, Dense, Reshape
-from keras.regularizers import l2
->>>>>>> 04ef0b7931a4aca2eea9024d07819f99f77aa03b
 
 from .base import AUDIO_DIR, AUDIO_META_DIR, preprocess
 
@@ -33,7 +28,6 @@ def train(num_epochs, batch_size, learning_rate, job_dir):
 
     keras.backend.clear_session()
 
-<<<<<<< HEAD
     #model = keras.Sequential()
 
     model = keras.Sequential([RandomFourierFeatures(output_dim=4096, scale=10.0, kernel_initializer="gaussian")])
@@ -43,34 +37,14 @@ def train(num_epochs, batch_size, learning_rate, job_dir):
     model.add(Dense(labels_onehot.shape[1], kernel_regularizer=regularizers.l2(0.01), activation="linear"))
 
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3), loss='hinge', metrics=['accuracy'])
-=======
-    model = keras.Sequential()
-
-    model.add(Dense(100, input_shape=loader.shape, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, kernel_regularizer=l2(0.01), activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(labels_onehot.shape[1], activation="sigmoid"))
-
-    model.compile(optimizer='adadelta', loss='squared_hinge', metrics=['accuracy'])
->>>>>>> 04ef0b7931a4aca2eea9024d07819f99f77aa03b
 
     history = model.fit_generator(SampleLoader(train, batch_size=batch_size), train.size/batch_size, epochs=num_epochs, **params)
 
     model.save(os.path.join(job_dir, 'model-export'), save_format='tf')
 
-<<<<<<< HEAD
     acc = model.evaluate_generator(SampleLoader(val, batch_size=100), val.size, **params)
     
     print( "**** VAL ACC ****")
-    print(acc.keys())
+    print(acc)
 
     history_dict = history.history
-
-
-=======
-    history_dict = history.history
-
-    print(history_dict.keys())
->>>>>>> 04ef0b7931a4aca2eea9024d07819f99f77aa03b
-
